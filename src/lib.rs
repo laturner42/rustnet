36c42727932618ae43d38a093351b795bf3314d2
HiroLord <presto4132@comcast.net>
2015-01-31 22:54:21 -0500

    Added some initialization functions.

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,8 @@ pub struct NetworkData{
     socket: sdl2_net::TCPsocket,
 }
 
-pub fn rnet_init_server(port: u8, num_clients: u32) -> Option<NetworkData> {
-    let possible_ss = rnet_initialize(1000);
+pub fn rnet_init_server(port: u16, num_clients: u32) -> Option<NetworkData> {
+    let possible_ss = rnet_initialize(num_clients as i32);
     
     let socket_set: sdl2_net::SocketSet;
 
@@ -22,7 +22,42 @@ pub fn rnet_init_server(port: u8, num_clients: u32) -> Option<NetworkData> {
         Some(ss) => socket_set = ss,
     }
 
-    let possible_ip = sdl2_net::become_host(1234);
+    let possible_ip = sdl2_net::become_host(port);
+
+    let mut ip: sdl2_net::IPaddress;
+
+    match possible_ip {
+        Some(i) => ip = i,
+        None => return None,
+    }
+
+    let possible_socket = sdl2_net::tcp_open(&mut ip);
+
+    let mut socket: sdl2_net::TCPsocket;
+
+    match possible_socket {
+        Some(s) => socket = s,
+        None => return None,
+    }
+
+    sdl2_net::add_socket(&socket_set, &socket);
+
+    Some(NetworkData{   write_buffer : [0; 512],    buffer_index : 0,
+                        read_buffer : [0; 512],     read_buffer_size : 0,
+                        socket_set : socket_set,    socket : socket})
+}
+
+pub fn rnet_init_client(host: &str, port: u16) -> Option<NetworkData> {
+    let possible_ss = rnet_initialize(1);
+
+    let socket_set: sdl2_net::SocketSet;
+
+    match possible_ss{
+        None => return None,
+        Some(ss) => socket_set = ss,
+    }
+
+    let possible_ip = sdl2_net::resolve_host(host, port);
 
     let mut ip: sdl2_net::IPaddress;
 
